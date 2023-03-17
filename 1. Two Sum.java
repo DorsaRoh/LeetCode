0469aeafab4303392bda28498a5661dc5c4e7593
Dorsa Rohani <dorsa.rohani@gmail.com>
2023-03-17 18:07:50 -0400

    Update 1. Two Sum.java

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -1,19 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int i1=0;
-        int i2=0;
+        int[] arr = new int[2];
         for(int i = 0; i < nums.length; i++){
-            for(int k = 1; k < nums.length; k++){
-                if(nums[i]+nums[k]==target && i != k){
-                    i1 = k;
-                    i2 = i;
+            for(int j = 0; j < nums.length; j++){
+                if(nums[i] + nums[j] == target && i != j){
+                    arr[0] = i;
+                    arr[1] = j;
                 }
             }
         }
-        int arr[] = new int[2];
-        arr[1] = i1;
-        arr[0] = i2; 
         return arr;
     }
 }
-
